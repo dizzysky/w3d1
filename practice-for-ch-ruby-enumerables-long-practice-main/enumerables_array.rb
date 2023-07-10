471e0e69e44901550b2cc573ac54e7141bff969a
Md Miah <mdmiah@mds-macbook-pro.mynetworksettings.com>
2023-07-10 16:58:02 -0400

    added changes

--- practice-for-ch-ruby-enumerables-long-practice-main/enumerables_array.rb
+++ practice-for-ch-ruby-enumerables-long-practice-main/enumerables_array.rb
@@ -15,6 +15,38 @@ class Array
     end
     result
   end
-end
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
+  def my_zip(*args)
+    result = Array.new(self.length) { Array.new(args.length + 1, nil) }
+    self.length.times do |i|
+      sub_array = [self[i]]
+      args.each do |array|
+        sub_array << array[i]
+      end
+      result[i] = sub_array
+    end
+    result
+  end
+
+  def my_rotate(num = 1)
+    result = []
+    total_rotations = num % self.length
+
+    select!.drop(total_rotations) + self.take(total_rotations)
+  end
+
+  def my_join(separator = "")
+    result = ""
+    result = self.inject() do |accum, el|
+      accum += separator + el
+    end
+  end
+
+  def my_reverse
+    result = []
+    self.each do |el|
+      result.prepend(el)
+    end
+    result
+  end
+end
